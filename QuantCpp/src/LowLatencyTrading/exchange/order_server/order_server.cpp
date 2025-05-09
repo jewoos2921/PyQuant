0a97712c6b05ccc5beada62e1403428267b977b1
jewoos2921 <jewoos2921@gmail.com>
2025-05-09 10:57:02 +0900

    수정중

---
+++ QuantCpp/src/LowLatencyTrading/exchange/order_server/order_server.cpp
@@ -0,0 +1,9 @@
+//
+// Created by jewoo on 2025-05-09.
+//
+
+#include "order_server.hpp"
+
+namespace QuantCpp::Exchange {
+
+}
---
+++ QuantCpp/src/LowLatencyTrading/exchange/order_server/order_server.hpp
@@ -0,0 +1,17 @@
+//
+// Created by jewoo on 2025-05-09.
+//
+
+#pragma once
+
+#include <functional>
+#include "common/thread_utils.hpp"
+#include "common/macros.hpp"
+#include "common/tcp_server.hpp"
+#include "order_server/client_request.hpp"
+#include "order_server/client_response.hpp"
+#include "order_server/fifo_sequencer.hpp"
+
+namespace QuantCpp::Exchange {
+
+}
