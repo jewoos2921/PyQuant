f9218c2eab1dba4f6da3b420675a84a275644152
jewoos2921 <jewoos2921@gmail.com>
2025-06-04 09:14:39 +0900

    feature_engine.h완료

--- QuantCpp/src/LowLatencyTrading/trading/strategy/feature_engine.hpp
+++ QuantCpp/src/LowLatencyTrading/trading/strategy/feature_engine.hpp
@@ -6,17 +6,18 @@
 using namespace QuantCpp::Common;
 namespace QuantCpp::Trading
 {
-    constexpr auto FeatureINVALID = std::numeric_limits<double>::quiet_NaN();
+    constexpr auto Feature_INVALID = std::numeric_limits<double>::quiet_NaN();
     class FeatureEngine
     {
     public:
         FeatureEngine(Logger *logger) : logger_(logger) {}
+
         FeatureEngine() = delete;
         // Disable copy and move semantics
         FeatureEngine(const FeatureEngine &) = delete;
-        FeatureEngine(FeatureEngine &&) = delete;
+        FeatureEngine(const FeatureEngine &&) = delete;
         FeatureEngine &operator=(const FeatureEngine &) = delete;
-        FeatureEngine &operator=(FeatureEngine &&) = delete;
+        FeatureEngine &operator=(const FeatureEngine &&) = delete;
 
         auto getMktPrice() const noexcept -> double
         {
@@ -31,13 +32,41 @@ namespace QuantCpp::Trading
         auto onOrderBookUpdate(TickerId ticker_id, Price price,
                                Side side, MarketOrderBook *book) noexcept -> void
         {
+             const auto bbo = book->getBBO();
+            if (LIKELY(bbo->bid_price_ != Price_INVALID &&
+                       bbo->ask_price_ != Price_INVALID))
+            {
+              mkt_price_ = (bbo->bid_price_ * bbo->ask_qty_ + bbo->ask_price_ * bbo->bid_qty_) /
+                           static_cast<double>(bbo->bid_qty_ + bbo->ask_qty_);
+            }
+            logger_->log("%:% %() % ticker:% price:% side:% mkt-price:% agg-trade-ratio:%\n",
+                         __FILE__, __LINE__, __FUNCTION__,
+                         getCurrentTimeStr(&time_str_),ticker_id, priceToString(price).c_str(),
+                         sideToString(side).c_str(), mkt_price_, agg_trade_qty_ratio_);
+                         
+        }
+
+        auto onTradeUpdate(const Exchange::MEMarketUpdate *market_update,
+                           MarketOrderBook *book) noexcept -> void
+        {
+            const auto bbo = book->getBBO();
+            if (LIKELY(bbo->bid_price_ != Price_INVALID &&
+                       bbo->ask_price_ != Price_INVALID))
+            {
+                agg_trade_qty_ratio_ = static_cast<double>(market_update->qty_) /
+                                       (market_update->side_ == Side::BUY ? bbo->ask_qty_ : bbo->bid_qty_);
+            }
+            logger_->log("%:% %() % % mkt-price:% agg-trade-ratio:%\n",
+                         __FILE__, __LINE__, __FUNCTION__,
+                         getCurrentTimeStr(&time_str_),
+                         market_update->toString().c_str(), mkt_price_, agg_trade_qty_ratio_);
         }
 
     private:
         std::string time_str_;
         Logger *logger_ = nullptr;
 
-        double mkt_price_ = FeatureINVALID;
-        double agg_trade_qty_ratio_ = FeatureINVALID;
+        double mkt_price_ = Feature_INVALID;
+        double agg_trade_qty_ratio_ = Feature_INVALID;
     };
 } // namespace QuantCpp::Trading
--- QuantCpp/src/LowLatencyTrading/trading/strategy/market_order_book.hpp
+++ QuantCpp/src/LowLatencyTrading/trading/strategy/market_order_book.hpp
@@ -13,20 +13,47 @@ namespace QuantCpp::Trading
     class MarketOrderBook final
     {
     public:
+        MarketOrderBook(TickerId ticker_id, Logger *logger);
+        ~MarketOrderBook();
+        auto onMarketUpdate(const Exchange::MEMarketUpdate *market_update) noexcept -> void;
+        auto setTradeEngine(TradeEngine *trade_engine);
+        auto updateBBO(bool update_bid, bool update_ask) noexcept;
+        auto getBBO() const noexcept -> const BBO *;
+        auto toString(bool detailed, bool validity_check) const -> std::string;
+
         MarketOrderBook() = delete;
         // Disable copy and move semantics
         MarketOrderBook(const MarketOrderBook &) = delete;
-        MarketOrderBook(MarketOrderBook &&) = delete;
+        MarketOrderBook(const MarketOrderBook &&) = delete;
         MarketOrderBook &operator=(const MarketOrderBook &) = delete;
-        MarketOrderBook &operator=(MarketOrderBook &&) = delete;
+        MarketOrderBook &operator=(const MarketOrderBook &&) = delete;
 
     private:
         const TickerId ticker_id_;
         TradeEngine *trade_engine_ = nullptr;
         OrderHashMap oid_to_order_;
 
-        Logger *logger_ = nullptr;
+        MemPool<MarketOrdersAtPrice> orders_at_price_pool_;
+        MarketOrdersAtPrice *bids_by_price_ = nullptr;
+        MarketOrdersAtPrice *asks_by_price_ = nullptr;
 
+        OrdersAtPriceHashMap price_orders_at_price_;
+
+        MemPool<MarketOrder> order_pool_;
+        BBO bbo_;
+
+        Logger *logger_ = nullptr;
+        std::string time_str_;
         OrdersAtPriceHashMap orders_at_price_map_;
+
+    private:
+        auto priceToIndex(Price price) const noexcept;
+        auto getOrdersAtPrice(Price price) noexcept -> MarketOrdersAtPrice *;
+        auto addOrdersAtPrice(MarketOrdersAtPrice *new_orders_at_price) noexcept;
+        auto removeOrdersAtPrice(Side side, Price price) noexcept  ;
+        auto removeOrder(MarketOrder *order) noexcept -> void;
+        auto addOrder(MarketOrder *order) noexcept -> void;
     };
+
+    using MarketOrderBookHashMap = std::array<MarketOrderBook *, ME_MAX_TICKERS>; // Example size, adjust as needed
 } // namespace QuantCpp::Trading
